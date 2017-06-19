8ab19977610fe16f1785834986dcf018434379ac
adrianliaw <adrianliaw2000@gmail.com>
2017-06-20 02:50:10 +0800

    Add configurable lazy-loaded mongo connections

--- web-service/app.py
+++ web-service/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify
-from configparser import ConfigParser
-from pathlib import Path
+from .config import get_config
+from .db import configdb, metricdb
 
 
 app = Flask(__name__)
 
-config = ConfigParser()
-config.read(Path(__file__).absolute().parent.parent / "config.ini")
-app.config.update(config)
+app.config.update(get_config())
 
 
 @app.route("/")
 def index():
     return jsonify(status="ok")
+
+
+@app.route("/single-app/services/<app_name>")
+def services_json(app_name):
+    app = configdb.applications.find_one({"name": app_name})
+    return jsonify(app=app["name"], services=app["serviceNames"])
---
+++ web-service/db.py
@@ -0,0 +1,35 @@
+import logging
+from pymongo import MongoClient
+from pymongo.errors import OperationFailure
+from .config import get_config
+
+
+config = get_config()
+
+client = MongoClient(
+    host=config["MONGODB"]["HOST"],
+    port=config["MONGODB"].getint("PORT"),
+)
+
+class Database(object):
+    def __init__(self, name):
+        self.name = name
+        self.auth = (config["MONGODB"]["USERNAME"],
+                     config["MONGODB"]["PASSWORD"])
+        self._database = None
+
+    def _get_database(self):
+        if self._database is not None:
+            return self._database
+        db = client.get_database(self.name)
+        db.authenticate(*self.auth)
+        return db
+
+    def __getattribute__(self, attribute):
+        try:
+            return super().__getattribute__(attribute)
+        except AttributeError:
+            return getattr(self._get_database(), attribute)
+
+configdb = Database(config["ANALYZER"]["CONFIGDB_NAME"])
+metricdb = Database(config["ANALYZER"]["METRICDB_NAME"])
