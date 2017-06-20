7e64c08d65f6670b10a881156a5bbb5aa286932a
adrianliaw <adrianliaw2000@gmail.com>
2017-06-20 16:18:03 +0800

    Add profiling data API

--- api-service/app.py
+++ api-service/app.py
@@ -28,15 +28,25 @@ def get_available_apps():
 
 @app.route("/single-app/services/<app_name>")
 def services_json(app_name):
-    app = configdb.applications.find_one({"name": app_name})
+    app = configdb.applications.find_one(
+        filter={"name": app_name},
+        projection={"_id": 0, "name": 1, "serviceNames": 1},
+    )
     return jsonify(app=app["name"], services=app["serviceNames"])
+
+@app.route("/single-app/profiling/<objectid:app_id>")
+def profiling_json(app_id):
+    app = metricdb.profiling.find_one(app_id)
+    return jsonify(app)
 
 @app.route("/cross-app/predict", methods=["POST"])
 def predict():
     body = request.get_json()
-    if body["model"] == "LinearRegression1":
+    if body.get("model") == "LinearRegression1":
         model = models.LinearRegression1(num_dims=3)
         result = model.fit(None, None).predict(body["app_1"], body["app_2"])
         return jsonify(result.to_dict())
     else:
-        abort(make_response(jsonify(error="Model not found")), 404)
+        response = jsonify(error="Model not found")
+        response.status_code = 404
+        return response
