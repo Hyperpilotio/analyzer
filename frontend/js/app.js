b8b283a70ca2fde6d9fffff07a95459e3d6edec9
adrianliaw <adrianliaw2000@gmail.com>
2017-06-22 01:37:58 +0800

    Add basic calibration graph

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,56 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "babel-polyfill";
+import "whatwg-fetch";
+import React, { Component } from "react";
+import ReactEcharts from "echarts-for-react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<span>Hello world</span>, document.getElementById("react-root"));
+
+class CalibrationChart extends Component {
+
+  constructor() {
+    super();
+    this.state = { option: {series: []} };
+  }
+
+  onChartReady(chart) {
+    this.chart = chart;
+  }
+
+  async componentDidMount() {
+    const res = await fetch("/api/single-app/calibration-data/59406aa9e3fd9e5094db7f3b");
+    const data = await res.json();
+    this.setState({ option: {
+      tooltip: {
+        trigger: "axis",
+        formatter: (params) => {
+          params = params[0];
+          return `Load Intesity: ${params.axisValue}<br />${params.marker} ${params.data[1].toFixed(2)}`;
+        }
+      },
+      xAxis: {
+        type: "value"
+      },
+      yAxis: {
+        type: "value",
+        min: 10000,
+        max: 50000
+      },
+      series: [
+        {
+          name: "throughput",
+          type: "line",
+          data: data.map(row => [row.loadIntensity, row.mean])
+        }
+      ]
+    } });
+    this.chart.hideLoading();
+  }
+
+  render() {
+    return <ReactEcharts style={{height: "500px"}} onChartReady={this.onChartReady.bind(this)} option={this.state.option} showLoading={true} />;
+  }
+
+}
+
+
+ReactDOM.render(<CalibrationChart />, document.getElementById("react-root"));
