8b2ec50ab40cc70897f0c922464cd8ea3e63f140
adrianliaw <adrianliaw2000@gmail.com>
2017-07-05 15:09:02 +0800

    Refactor Sass, use colour variables, add font

--- frontend/components/HeaderNav.js
+++ frontend/components/HeaderNav.js
@@ -6,21 +6,21 @@ export default () => (
       <div className="container">
 
         {/* HyperPilot icon */}
-        <div className="nav-left">
+        <div className="left">
           <div className="navbar-item">
             <img className="brand" src="http://placehold.it/40/40" />
           </div>
         </div>
 
         {/* Search bar */}
-        <div className="nav-center">
+        <div className="center">
           <div className="navbar-item">
             <input type="search" placeholder="Jump to apps, status, services..." />
           </div>
         </div>
 
         {/* Menu and account icon */}
-        <div className="nav-right nav-sublist">
+        <div className="right nav-sublist">
           <div className="navbar-item">
             <img className="menu-icon" src="http://placehold.it/40/40" />
           </div>
