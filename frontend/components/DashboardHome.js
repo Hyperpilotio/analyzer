1e8c097e426e3d4c5b3588aef7795bd75353fcbb
adrianliaw <adrianliaw2000@gmail.com>
2017-07-05 17:28:49 +0800

    Add current status indicators

--- frontend/components/DashboardHome.js
+++ frontend/components/DashboardHome.js
@@ -19,6 +19,30 @@ export default () => (
           <div className="column info-list">
             <div className="info-section">
               <div className="info-key">Current Status</div>
+              <div className="info-value status-indicator-list">
+                <div className="columns">
+                  <div className="column status-indicator">
+                    <div>
+                      <div className="key-stat">35</div>
+                      <div className="key-stat-label">apps</div>
+                    </div>
+                    <span className="badge success">Healthy</span>
+                  </div>
+                  <div className="column status-indicator">
+                    <div>
+                      <div className="key-stat">12</div>
+                      <div className="key-stat-label">apps</div>
+                    </div>
+                    <span className="badge danger">Interfering</span>
+                  </div>
+                  <div className="column status-indicator">
+                    <div>
+                      <div className="key-stat">36.5%</div>
+                      <div className="key-stat-label">average QoS rate</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
