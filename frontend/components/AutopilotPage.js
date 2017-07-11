93634497536fbf32526c11c9dcd84e631b5d24e6
adrianliaw <adrianliaw2000@gmail.com>
2017-07-12 03:03:57 +0800

    Add autopilot page for 'after applying recommendation'

--- frontend/components/AutopilotPage.js
+++ frontend/components/AutopilotPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
 import MetricScoreChart from "./MetricScoreChart";
 import redisLogo from "../assets/images/asset_redis_logo.svg";
 import mongoLogo from "../assets/images/asset_mongoDB_logo.svg";
@@ -76,8 +77,17 @@ export default () => (
               </div>
             </section>
           </header>
-          <footer className="only-button">
-            <a className="primary-button" href="#">Apply Recommendation</a>
+          <footer>
+            <Switch>
+              <Route path="/autopilot/after">
+                <p>Placement recommendation applied!</p>
+              </Route>
+              <Route path="/autopilot">
+                <Link to="/autopilot/after" className="primary-button">
+                  Apply Recommendation
+                </Link>
+              </Route>
+            </Switch>
           </footer>
         </div>
       </article>
@@ -164,13 +174,98 @@ export default () => (
 
       </article>
 
-      <article className="column">
-        <h3>Optimized QoS Score</h3>
-        <div className="run-optimizer-mask">
-          <p>Apply recommendation to see enhanced QoS metric</p>
-          <a className="primary-button" href="#">Apply Recommendation</a>
-        </div>
-      </article>
+        <Switch>
+          <Route path="/autopilot/after">
+            <article className="column">
+              <h3>QoS Score After Autopilot</h3>
+              <section>
+                <span className="app-title">
+                  <img src={redisLogo} />
+                  <h4>Redis</h4>
+                </span>
+                <div className="score-chart-box">
+                  <header>
+                    <span className="left">Latency</span>
+                    <div className="right columns">
+                      <div className="column status-indicator">
+                        <div className="key-stat">245</div>
+                        <div className="key-stat-label">Current</div>
+                      </div>
+                      <div className="column status-indicator">
+                        <div className="key-stat danger">500</div>
+                        <div className="key-stat-label">Target</div>
+                      </div>
+                    </div>
+                  </header>
+                  <main>
+                    <MetricScoreChart name="Latency" thresholdColor="#ff8686" />
+                  </main>
+                </div>
+              </section>
+
+              <section>
+                <span className="app-title">
+                  <img src={mongoLogo} />
+                  <h4>MongoDB</h4>
+                </span>
+                <div className="score-chart-box">
+                  <header>
+                    <span className="left">Throughput</span>
+                    <div className="right columns">
+                      <div className="column status-indicator">
+                        <div className="key-stat">338</div>
+                        <div className="key-stat-label">Current</div>
+                      </div>
+                      <div className="column status-indicator">
+                        <div className="key-stat success">500</div>
+                        <div className="key-stat-label">Target</div>
+                      </div>
+                    </div>
+                  </header>
+                  <main>
+                    <MetricScoreChart name="Throughput" thresholdColor="#7ed321" />
+                  </main>
+                </div>
+              </section>
+
+              <section>
+                <span className="app-title">
+                  <img src={kafkaLogo} />
+                  <h4>Kafka</h4>
+                </span>
+                <div className="score-chart-box">
+                  <header>
+                    <span className="left">Throughput</span>
+                    <div className="right columns">
+                      <div className="column status-indicator">
+                        <div className="key-stat">254</div>
+                        <div className="key-stat-label">Current</div>
+                      </div>
+                      <div className="column status-indicator">
+                        <div className="key-stat success">500</div>
+                        <div className="key-stat-label">Target</div>
+                      </div>
+                    </div>
+                  </header>
+                  <main>
+                    <MetricScoreChart name="Throughput" thresholdColor="#7ed321" />
+                  </main>
+                </div>
+              </section>
+            </article>
+
+          </Route>
+
+          <Route path="/autopilot/">
+            <article className="column">
+              <h3>QoS Score After Autopilot</h3>
+              <div className="run-optimizer-mask">
+                <p>Apply recommendation to see enhanced QoS metric</p>
+                <Link to="/autopilot/after" className="primary-button">Apply Recommendation</Link>
+              </div>
+            </article>
+          </Route>
+        </Switch>
     </div>
   </div>
 )
