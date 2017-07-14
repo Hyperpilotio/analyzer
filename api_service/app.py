508fd0f52da0511594b513dc448b7ceb6f662a81
adrianliaw <adrianliaw2000@gmail.com>
2017-07-15 03:44:25 +0800

    Remove unused API endpoints

--- api_service/app.py
+++ api_service/app.py
@@ -12,9 +12,11 @@ app.config.update(get_config())
 app.json_encoder = JSONEncoderWithMongo
 app.url_map.converters["objectid"] = ObjectIdConverter
 
+
 @app.route("/")
 def index():
     return jsonify(status="ok")
+
 
 @app.route("/apps")
 def get_all_apps():
@@ -24,10 +26,12 @@ def get_all_apps():
         apps[str(app_id)] = app
     return jsonify(apps)
 
+
 @app.route("/apps/<objectid:app_id>")
 def get_app_info(app_id):
     app = configdb.applications.find_one(app_id, {"_id": 0})
     return ensure_document_found(app)
+
 
 @app.route("/apps/<objectid:app_id>/calibration")
 def app_calibration(app_id):
@@ -48,6 +52,7 @@ def app_calibration(app_id):
     except StopIteration:
         return ensure_document_found(None)
 
+
 @app.route("/apps/<objectid:app_id>/profiling")
 def app_profiling(app_id):
     app = configdb.applications.find_one(app_id)
@@ -67,6 +72,7 @@ def app_profiling(app_id):
     except StopIteration:
         return ensure_document_found(None)
 
+
 @app.route("/apps/<objectid:app_id>/interference")
 def interference_scores(app_id):
     app = configdb.applications.find_one(app_id)
@@ -84,49 +90,6 @@ def interference_scores(app_id):
     except StopIteration:
         return ensure_document_found(None)
 
-@app.route("/available-apps")
-def get_available_apps():
-    return jsonify({
-        collection: metricdb[collection].find(
-            filter={"appName": {"$exists": 1}},
-            projection={"appName": 1},
-        )
-        for collection in ("calibration", "profiling", "validation")
-    })
-
-@app.route("/single-app/services/<app_name>")
-def services_json(app_name):
-    app_config = configdb.applications.find_one(
-        filter={"name": app_name},
-        projection={"_id": 0, "name": 1, "serviceNames": 1},
-    )
-    return ensure_document_found(app_config, app="name", services="serviceNames")
-
-@app.route("/single-app/profiling/<objectid:app_id>")
-def profiling_json(app_id):
-    profiling = metricdb.profiling.find_one(app_id)
-    return ensure_document_found(profiling)
-
-@app.route("/single-app/profiling-data/<objectid:app_id>")
-def profiling_data(app_id):
-    profiling = metricdb.profiling.find_one(app_id)
-    data = get_profiling_dataframe(profiling)
-    if data is not None:
-        profiling["testResult"] = data
-    return ensure_document_found(profiling)
-
-@app.route("/single-app/calibration/<objectid:app_id>")
-def calibration_json(app_id):
-    calibration = metricdb.calibration.find_one(app_id)
-    return ensure_document_found(calibration)
-
-@app.route("/single-app/calibration-data/<objectid:app_id>")
-def calibration_data(app_id):
-    calibration = metricdb.calibration.find_one(app_id)
-    data = get_calibration_dataframe(calibration)
-    if data is not None:
-        calibration["testResult"] = data
-    return ensure_document_found(calibration)
 
 @app.route("/cross-app/predict", methods=["POST"])
 def predict():
@@ -143,13 +106,3 @@ def predict():
         response = jsonify(error="Model not found")
         response.status_code = 404
         return response
-
-@app.route("/radar-data/<objectid:app_id>")
-def radar_data(app_id):
-    profiling = metricdb.profiling.find_one(app_id)
-    data = get_radar_dataframe(profiling)
-    if data is not None:
-        profiling['radarChartData'] = data
-        del profiling['testResult']
-
-    return ensure_document_found(profiling)
