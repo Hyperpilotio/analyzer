50a2a5a0ccb5dd0ca1de80842d04ae4435e721ef
adrianliaw <adrianliaw2000@gmail.com>
2017-07-19 18:08:46 +0800

    Add ServicePlacement component that renders service placement from input data

---
+++ frontend/components/ServicePlacement.js
@@ -0,0 +1,31 @@
+import React from "react";
+import _ from "lodash";
+import redisLogo from "../assets/images/asset_redis_logo.svg";
+
+const Node = ({ id, instanceType, services }) => (
+  <section>
+    <h4>Node { id }</h4>
+    <div className="services-on-node">
+      { services.map( service => (
+        <div className="running-service">
+          <img src={redisLogo} />
+          <span>{ service.task }</span>
+        </div>
+      ) ) }
+    </div>
+  </section>
+)
+
+export default ({ className, title, footer, placement }) => (
+  <article className={className}>
+    <h3>{ title }</h3>
+    <div className="service-placement">
+      <header>
+        { placement.clusterDefinition.nodes.map(node => (
+          <Node {...node} services={ _.filter(placement.nodeMapping, ["id", node.id]) } />
+        )) }
+      </header>
+      { footer }
+    </div>
+  </article>
+)
