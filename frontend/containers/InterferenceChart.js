65df7460ebd157415bb9c1faaf67b56a408d0064
adrianliaw <adrianliaw2000@gmail.com>
2017-07-21 01:26:05 +0800

    Use newer interference score API to get interference scores for each service

--- frontend/containers/InterferenceChart.js
+++ frontend/containers/InterferenceChart.js
@@ -12,24 +12,24 @@ export default class InterferenceChart extends Component {
 
   state = { data: null, loading: true };
 
-  async fetchData(appId) {
-    if (_.isUndefined(_.get(this.context.store.interferences, appId))) {
-      await this.context.actions.fetchInterference(appId);
+  async fetchData(appId, serviceName) {
+    if (!_.has(this.context.store.interferences, `${appId}-${serviceName}`)) {
+      await this.context.actions.fetchInterference(appId, serviceName);
     }
     this.setState({
-      data: this.context.store.interferences[appId],
+      data: this.context.store.interferences[`${appId}-${serviceName}`],
       loading: false
     });
   }
 
   componentDidMount() {
-    this.fetchData(this.props.appId);
+    this.fetchData(this.props.appId, this.props.serviceName);
   }
 
   componentWillReceiveProps(props) {
-    if (props.appId !== this.props.appId) {
+    if (props.appId !== this.props.appId || props.serviceName !== this.props.serviceName) {
       this.setState({loading: true});
-      this.fetchData(props.appId);
+      this.fetchData(props.appId, props.serviceName);
     }
   }
 
