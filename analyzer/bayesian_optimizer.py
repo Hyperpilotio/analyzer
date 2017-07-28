4a3831825d103a753fcaee3ca9111e8730124fa6
Che-Yuan Liang <cheyuanl@andrew.cmu.edu>
2017-07-28 16:13:22 -0700

    Prune variables

--- analyzer/bayesian_optimizer.py
+++ analyzer/bayesian_optimizer.py
@@ -14,33 +14,39 @@ class UtilityFunction(object):
     """ An object to compute the acquisition functions.
     """
 
-    def __init__(self, kind, xi=0.0, kappa=5.):
+    def __init__(self, kind, gp_objective, gp_constraint=None, constraint_upper=None, xi=0.0, kappa=5.):
         """ If UCB is to be used, a constant kappa is needed.
         """
-        self.xi = xi
-        self.kappa = kappa
-        self.implementations = ['ucb', 'ei',
-                                'cei_analytic', 'cei_numeric', 'poi']
-
+        self.implementations = ['ucb', 'ei', 'cei', 'poi']
         if kind not in self.implementations:
-            err = f"The utility function " \
-                "{kind} has not been implemented, " \
-                "please choose one of ucb, ei, or poi."
+            err = f"The utility function {kind} has not been implemented, " \
+                "please choose one of ucb, ei, cei, or poi."
             raise NotImplementedError(err)
         else:
             self.kind = kind
 
-    def utility(self, x, gp_objective, y_max, gp_constraint=None, constraint_upper=None):
+        self.gp_objective = gp_objective
+        self.gp_constraint = gp_constraint
+        self.xi = xi
+        self.kappa = kappa
+        self.constraint_upper = constraint_upper
+
+    def utility(self, x):
+        gp_objective = self.gp_objective
+        gp_constraint = self.gp_constraint
+        constraint_upper = self.constraint_upper
+        xi, kappa = self.xi, self.kappa
+
         if self.kind == 'ucb':
-            return UtilityFunction._ucb(x, gp_objective, self.kappa)
+            return UtilityFunction._ucb(x, gp_objective, kappa)
         if self.kind == 'ei':
-            return UtilityFunction._ei(x, gp_objective, y_max, self.xi)
+            return UtilityFunction._ei(x, gp_objective, xi)
         if self.kind == 'cei':
             assert gp_constraint is not None, 'gaussian processor of constraint must be provided'
             assert constraint_upper is not None, 'constraint_upper must be provided'
-            return UtilityFunction._cei(x, gp_objective, y_max, self.xi, gp_constraint, constraint_upper)
+            return UtilityFunction._cei(x, gp_objective, xi, gp_constraint, constraint_upper)
         if self.kind == 'poi':
-            return UtilityFunction._poi(x, gp_objective, y_max, self.xi)
+            return UtilityFunction._poi(x, gp_objective, xi)
 
     @staticmethod
     def _ucb(x, gp_objective, kappa):
@@ -48,30 +54,34 @@ class UtilityFunction(object):
         return mean + kappa * std
 
     @staticmethod
-    def _ei(x, gp_objective, y_max, xi):
+    def _ei(x, gp_objective, xi):
+        y_max = gp_objective.y_train_.max()
         mean, std = gp_objective.predict(x, return_std=True)
         z = (mean - y_max - xi) / std
         return (mean - y_max - xi) * norm.cdf(z) + std * norm.pdf(z)
 
     @staticmethod
-    def _cei(x, gp_objective, y_max, xi, gp_constraint, constraint_upper):
+    def _cei(x, gp_objective, xi, gp_constraint, constraint_upper):
         """ Compute the cdf under constraint_upper (i.e. P(c(x) < constraint_upper)) modualte the ei(x).
             whereas c(x) is a estimated gaussian distribution.
         """
-        ei = UtilityFunction._ei(x, gp_objective, y_max, xi)
+        y_max = gp_objective.y_train_.max()
+        ei = UtilityFunction._ei(x, gp_objective, xi)
+
         mean, std = gp_constraint.predict(x, return_std=True)
         z = (constraint_upper - mean) / std
         cumulative_probabiliy = norm.cdf(z)
         return cumulative_probabiliy * ei
 
     @staticmethod
-    def _poi(x, gp_objective, y_max, xi):
+    def _poi(x, gp_objective, xi):
+        y_max = gp_objective.y_train_.max()
         mean, std = gp_objective.predict(x, return_std=True)
         z = (mean - y_max - xi) / std
         return norm.cdf(z)
 
 
-def acq_max(ac, gp_objective, y_max, bounds, constraint_upper=None, mean=None, std=None, gp_constraint=None):
+def acq_max(utility, bounds):
     """ A function to find the maximum of the acquisition function
         It uses a combination of random sampling (cheap) and the 'L-BFGS-B'
         optimization method. First by sampling 1e5 points at random, and then
@@ -84,11 +94,11 @@ def acq_max(ac, gp_objective, y_max, bounds, constraint_upper=None, mean=None, s
     Returns
         x_max: The arg max of the acquisition function.
     """
+    
     # Warm up with random points
     x_tries = np.random.uniform(bounds[:, 0], bounds[:, 1],
                                 size=(100000, bounds.shape[0]))
-    ys = ac(x_tries, gp_objective=gp_objective, y_max=y_max,
-            constraint_upper=constraint_upper, mean=mean, std=std, gp_constraint=gp_constraint, bounds=bounds)
+    ys = utility(x_tries)
     x_max = x_tries[ys.argmax()]
     max_acq = ys.max()
 
@@ -97,9 +107,7 @@ def acq_max(ac, gp_objective, y_max, bounds, constraint_upper=None, mean=None, s
                                 size=(250, bounds.shape[0]))
     for x_try in x_seeds:
         # Find the minimum of minus the acquisition function
-        res = minimize(lambda x: -ac(x.reshape(1, -1), gp_objective=gp_objective, y_max=y_max,
-                                     constraint_upper=constraint_upper, mean=mean, std=std, gp_constraint=gp_constraint, bounds=bounds),
-                       x_try.reshape(1, -1),
+        res = minimize(lambda x: -utility(x.reshape(1, -1)), x_try.reshape(1, -1),
                        bounds=bounds,
                        method="L-BFGS-B")
 
@@ -114,7 +122,8 @@ def acq_max(ac, gp_objective, y_max, bounds, constraint_upper=None, mean=None, s
 
 
 def unique_rows(a):
-    """ A functions to trim repeated rows that may appear when optimizing. This is necessary to avoid the sklearn GP object from breaking
+    """ A functions to trim repeated rows that may appear when optimizing.
+        This is necessary to avoid the sklearn GP object from breaking
     Args:
         a(array): array to trim repeated rows from
     Return:
@@ -130,6 +139,19 @@ def unique_rows(a):
     ui[1:] = (diff != 0).any(axis=1)
 
     return ui[reorder]
+
+
+def get_fitted_gaussian_processor(X_train, y_train, **gp_params):
+    # Initialize gaussian process regressor
+    gp = GaussianProcessRegressor()
+    gp.set_params(**gp_params)
+    logger.debug('Instantiated gaussian processor for objective function:\n' + f'{gp}')
+    # Find unique rows of X to avoid GP from breaking
+    ur = unique_rows(X_train)
+    logger.debug(f"Fitting gaussian processor")
+    gp.fit(X_train[ur], y_train[ur])
+    logger.debug(f'Done.')
+    return gp
 
 
 def get_candidate(feature_mat, objective_arr, bounds, acq,
@@ -153,45 +175,20 @@ def get_candidate(feature_mat, objective_arr, bounds, acq,
     # Set boundary
     bounds = np.asarray(bounds)
 
-    # Initialize gaussian process regressor for objective function
-    gp_objective = GaussianProcessRegressor()
-    gp_objective.set_params(**gp_params)
-    logger.debug(f'Instantiated gaussian processor for objective function:\n' + '{gp_objective}')
+    gp_objective = get_fitted_gaussian_processor(
+        feature_mat, objective_arr, **gp_params)
+    if (constraint_arr is not None) and (constraint_upper is not None):
+        gp_constraint = get_fitted_gaussian_processor(
+            feature_mat, constraint_arr, **gp_params)
+    else:
+        gp_constraint, constraint_upper = None, None
 
     # Initialize utiliy function
-    util = UtilityFunction(kind=acq, kappa=kappa, xi=xi)
-
-    # Find unique rows of X to avoid GP from breaking
-    ur = unique_rows(feature_mat)
-    logger.debug(f"Fitting Objective Gaussian Processor Regressor: {gp_objective}")
-    gp_objective.fit(feature_mat[ur], objective_arr[ur])
-    logger.debug(f'Done.')
+    util = UtilityFunction(kind=acq, gp_objective=gp_objective, gp_constraint=gp_constraint,
+                           constraint_upper=constraint_upper, xi=xi, kappa=kappa)
 
     # Finding argmax of the acquisition function.
     logger.debug("Computing argmax of acquisition function")
-    if acq == 'cei_numeric':
-        assert constraint_upper is not None
-        assert constraint_arr is not None
-        gp_constraint = GaussianProcessRegressor()
-        gp_constraint.set_params(**gp_params)
-        logger.debug(f'Instantiated gaussian processor for constraint function:\n' + '{gp_objective}')
-        # Find unique rows of X to avoid GP from breaking
-        ur = unique_rows(feature_mat)
-        logger.debug(f"Fitting Constraint Gaussian Processor Regressor: {gp_constraint}")
-        gp_constraint.fit(feature_mat[ur], constraint_arr[ur])
-        logger.debug(f'Done.')
-        argmax = acq_max(ac=util.utility,
-                         gp_objective=gp_objective,
-                         y_max=objective_arr.max(),
-                         bounds=bounds,
-                         constraint_upper=constraint_upper,
-                         gp_constraint=gp_constraint
-                         )
-    else:
-        argmax = acq_max(ac=util.utility,
-                         gp_objective=gp_objective,
-                         y_max=objective_arr.max(),
-                         bounds=bounds,
-                         )
+    argmax = acq_max(utility=util.utility, bounds=bounds)
 
     return argmax
