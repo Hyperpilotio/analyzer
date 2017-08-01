e455069666d06f5f4e4d05464975dd075c07b5a7
Christos Kozyrakis <kozyraki@stanford.edu>
2017-08-01 13:34:30 -0700

    Various bug fixes

--- analyzer/analyzer_harness.py
+++ analyzer/analyzer_harness.py
@@ -20,6 +20,11 @@ import sys
 import time
 from . import util
 from . import bayesian_optimizer_pool
+
+# a couple of globals
+features = {}
+cloud = None
+
 class CloudPerf(object):
   """ A class for a cloud performance model
   """
@@ -30,7 +35,7 @@ class CloudPerf(object):
                mem_a, mem_b, mem_c, mem_w,
                net_a, net_b, net_c, net_w,
                io_a, io_b, io_c, io_w,
-               noise, nrange):
+               base, noise, nrange):
     """ initialize key parameters
     """
     self.vcpu_a = vcpu_a
@@ -55,6 +60,7 @@ class CloudPerf(object):
     self.io_w = io_w
     self.noise = noise
     self.nrange = nrange
+    self.base = base
     # some sanity checks
     if (vcpu_w + clk_w + mem_w + net_w + io_w) != 1.0:
       print("ERROR: Performance weights should sum to 1.0")
@@ -127,48 +133,75 @@ class CloudPerf(object):
     if noise and self.noise:
       change = randint((-1) * self.nrange, self.nrange)
       perf += perf*change/100
-    return perf
+    return self.base * perf
 
+
+def str2bool(v):
+  """ estimate performance
+  """
+  if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    return True
+  elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    return False
+  else:
+    raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def nodeinfo(nodetype):
+  """ returns a string with all the node info
+  """
+  global features
+  global cloud
+  if nodetype == "none":
+    return "perf 0.00, price 0.000, cost 0.00, perf/cost 0.00"
+  np_feat = features[nodetype]
+  feat = np_feat.astype(type('float', (float,), {}))
+  perf = cloud.perf(feat[0], feat[1], feat[2], feat[3], feat[4], False)
+  price = util.get_price(nodetype)
+  cost = util.compute_cost(price, 'throughput', perf)
+  return "perf %0.2f, price %0.3f, cost %0.2f, perf/cost %0.2f" \
+         %(perf, price, cost, perf/cost)
 
 def __main__():
   """ Main function of analyzer harness
   """
-
   # parse arguments
   parser = argparse.ArgumentParser()
-  parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+  parser.add_argument("-v", "--verbose", type=str2bool, required=False, default=False, help="increase output verbosity")
   parser.add_argument("-i", "--iter", type=int, required=False, default=10, help="maximum iterations")
-  parser.add_argument("-n", "--noise", help="add noise to cloud performance", action="store_false")
+  parser.add_argument("-n", "--noise", type=str2bool, required=False, default=True, help="add noise to cloud performance")
   parser.add_argument("-r", "--nrange", type=int, required=False, default=10, help="noise range (int)")
   parser.add_argument("-va", "-vcpua", type=float, required=False, default=1.0, help="vcpu a")
   parser.add_argument("-vb", "-vcpub", type=float, required=False, default=0.0, help="vcpu b")
   parser.add_argument("-vc", "-vcpuc", type=float, required=False, default=0.0, help="vcpu c")
-  parser.add_argument("-vw", "-vcpuw", type=float, required=False, default=0.2, help="vcpu w")
   parser.add_argument("-ca", "-clka", type=float, required=False, default=1.0, help="clk a")
   parser.add_argument("-cb", "-clkb", type=float, required=False, default=0.0, help="clk b")
   parser.add_argument("-cc", "-clkc", type=float, required=False, default=0.0, help="clk c")
-  parser.add_argument("-cw", "-clkw", type=float, required=False, default=0.2, help="clk w")
   parser.add_argument("-ma", "-mema", type=float, required=False, default=1.0, help="mem a")
   parser.add_argument("-mb", "-memb", type=float, required=False, default=0.0, help="mem b")
   parser.add_argument("-mc", "-memc", type=float, required=False, default=0.0, help="mem c")
-  parser.add_argument("-mw", "-memw", type=float, required=False, default=0.2, help="mem w")
   parser.add_argument("-na", "-neta", type=float, required=False, default=1.0, help="net a")
   parser.add_argument("-nb", "-netb", type=float, required=False, default=0.0, help="net b")
   parser.add_argument("-nc", "-netc", type=float, required=False, default=0.0, help="net c")
-  parser.add_argument("-nw", "-netw", type=float, required=False, default=0.2, help="net w")
   parser.add_argument("-ia", "-ioa", type=float, required=False, default=1.0, help="io a")
   parser.add_argument("-ib", "-iob", type=float, required=False, default=0.0, help="io b")
   parser.add_argument("-ic", "-ioc", type=float, required=False, default=0.0, help="io c")
-  parser.add_argument("-iw", "-iow", type=float, required=False, default=0.2, help="io w")
+  parser.add_argument("-vw", "-vcpuw", type=float, required=False, default=1.0, help="vcpu w")
+  parser.add_argument("-cw", "-clkw", type=float, required=False, default=0.0, help="clk w")
+  parser.add_argument("-mw", "-memw", type=float, required=False, default=0.0, help="mem w")
+  parser.add_argument("-nw", "-netw", type=float, required=False, default=0.0, help="net w")
+  parser.add_argument("-iw", "-iow", type=float, required=False, default=0.0, help="io w")
+  parser.add_argument("-b", "-base", type=float, required=False, default=1.0, help="base performance")
   args = parser.parse_args()
 
   # initialize performance model
-  cloud_perf = CloudPerf(args.va, args.vb, args.vc, args.vw, \
+  global cloud
+  global features
+  cloud = CloudPerf(args.va, args.vb, args.vc, args.vw, \
                          args.ca, args.cb, args.cc, args.cw, \
                          args.ma, args.mb, args.mc, args.mw, \
                          args.na, args.nb, args.nc, args.nw, \
                          args.ia, args.ib, args.ic, args.iw, \
-                         args.noise, args.nrange)
+                         args.b, args.noise, args.nrange)
   print("Running analyzer harness with following parameters:")
   print(args)
   print()
@@ -180,7 +213,6 @@ def __main__():
     print("ERROR: Not enough nodetypes in database")
     sys.exit()
   # build dictionary with features for all instances
-  features = {}
   for nodetype in all_nodetypes:
     name = nodetype['name']
     feat = util.encode_instance_type(name)
@@ -205,12 +237,15 @@ def __main__():
       if status_dict['status'] != "running":
         break
       time.sleep(1)
+    if args.verbose:
+      print("......Analyzer returned ", status_dict)
     # throw error if needed
     if status_dict["status"] != "done":
       print("ERROR: Analyzer returned with status %s"  %status_dict["status"])
       sys.exit()
     # termination
     if len(status_dict["data"]) == 0:
+      print("...Terminating due to empty reply")
       break
     # prepare next candidates
     request_str = "{\"appName\": \"redis\", \"data\": [ "
@@ -219,7 +254,7 @@ def __main__():
       count += 1
       np_feat = features[nodetype]
       feat = np_feat.astype(type('float', (float,), {}))
-      perf = cloud_perf.perf(feat[0], feat[1], feat[2], feat[3], feat[4], True)
+      perf = cloud.perf(feat[0], feat[1], feat[2], feat[3], feat[4], True)
       request_str += "{\"instanceType\": \"%s\", \"qosValue\": %f}" %(nodetype, perf)
       if count < len(status_dict["data"]):
         request_str += ", "
@@ -231,17 +266,22 @@ def __main__():
     request_str += "]}"
     request_dict = json.loads(request_str)
     analyzer.get_candidates("redis", request_dict)
+    if args.verbose:
+      print("......Called analyzer with arguments %s", request_str)
     time.sleep(1)
 
   # evaluate results
   slo = util.get_slo_value("redis")
-  budget = util.get_budget_value("redis")
+  budget = util.get_budget("redis")
   # scan all visited nodetypes
   visited_perfcost_i = visited_perf_i = visited_cost_i = "none"
-  visited_perfcost = visited_perf = visited_cost = -1.0
+  visited_perf = 0.0
+  visited_perfcost = 0.0
+  visited_cost = 1.79e+30
   for key in visited:
-    feat = features[key]
-    perf = cloud_perf.perf(feat[0], feat[1], feat[2], feat[3], feat[4], False)
+    np_feat = features[nodetype]
+    feat = np_feat.astype(type('float', (float,), {}))
+    perf = cloud.perf(feat[0], feat[1], feat[2], feat[3], feat[4], False)
     price = util.get_price(key)
     cost = util.compute_cost(price, 'throughput', perf)
     if perf > visited_perf and cost <= budget:
@@ -255,10 +295,13 @@ def __main__():
       visited_perfcost = perf/cost
   # scan all nodetypes
   cloud_perfcost_i = cloud_perf_i = cloud_cost_i = "none"
-  cloud_perfcost = cloud_perf = cloud_cost = -1.0
+  cloud_perfcost = 0.0
+  cloud_perf = 0.0
+  cloud_cost = 1.79e+30
   for key in features:
-    feat = features[key]
-    perf = cloud_perf.perf(feat[0], feat[1], feat[2], feat[3], feat[4], False)
+    np_feat = features[nodetype]
+    feat = np_feat.astype(type('float', (float,), {}))
+    perf = cloud.perf(feat[0], feat[1], feat[2], feat[3], feat[4], False)
     price = util.get_price(key)
     cost = util.compute_cost(price, 'throughput', perf)
     if perf > cloud_perf and cost <= budget:
@@ -276,7 +319,7 @@ def __main__():
   print("")
   print(".......................")
   print("... Analyzer results...")
-  print("Iterations requested): %d", args.iter)
+  print("Iterations requested): %d" %args.iter)
   print("Noise: ", args.noise)
   print("Noise range: ", args.nrange)
   print("Min perf: ", slo)
@@ -284,15 +327,15 @@ def __main__():
   print("Nodetypes examined: ", len(visited))
   print("")
   print("Performance/cost")
-  print("   Best avalailable: %s, performance/cost %f" %(cloud_perfcost_i, cloud_perfcost))
-  print("   Best found: %s, performance/cost %f" %(visited_perfcost_i, visited_perfcost))
+  print("   Best avalailable: %s, %s" %(cloud_perfcost_i, nodeinfo(cloud_perfcost_i)))
+  print("   Best found: %s, %s" %(visited_perfcost_i, nodeinfo(visited_perfcost_i)))
   print("")
   print("Perfomance with cost constraint")
-  print("   Best avalailable: %s, performance %f" %(cloud_perf_i, cloud_perf))
-  print("   Best found: %s, performance %f" %(visited_perf_i, visited_perf))
+  print("   Best avalailable: %s, %s" %(cloud_perf_i, nodeinfo(cloud_perf_i)))
+  print("   Best found: %s, %s" %(visited_perf_i, nodeinfo(visited_perf_i)))
   print("")
   print("Cost with performance constraint")
-  print("   Best avalailable: %s, cost %f" %(cloud_cost_i, cloud_cost))
-  print("   Best found: %s, cost %f" %(visited_cost_i, cloud_cost))
+  print("   Best avalailable: %s, %s" %(cloud_cost_i, nodeinfo(cloud_cost_i)))
+  print("   Best found: %s, %s" %(visited_cost_i, nodeinfo(visited_cost_i)))
 
 __main__()
