d6f3c435a018bc336598bcade5558d8505d54e42
adrianliaw <adrianliaw2000@gmail.com>
2017-08-02 15:34:42 -0700

    Remove frontend and legacy-web-service directories

--- legacy-web-service/crossApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    # predict Cross-App Interference Score matrix of two applications
-    url(r'^predict/$', views.predict, name='predict')
-]
--- legacy-web-service/singleApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^calibration/(?P<appId>[-\w]+)$',
-        views.calibration, name='calibration'),
-    url(r'^profiling/(?P<appId>[-\w]+)$',
-        views.profiling, name='profiling')
-]
