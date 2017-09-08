bbb6259efbb78d9d8e207ee56733dc1885bb1934
William Chen <chen.biwei@gmail.com>
2017-09-08 17:36:53 +0800

    Improve performance 1. remove unused config 2. use generator rather than list 3. change key list format 4. write result to keys.txt and data.txt

--- analyzer/data_source.py
+++ analyzer/data_source.py
@@ -1,7 +1,6 @@
 """Data Source for xgboost."""
 from influxdb import InfluxDBClient
 from datetime import datetime
-from math import fabs
 
 import json
 
@@ -30,14 +29,16 @@ def get_xgboost_data(app_metric, app_slo, tag_name, tag_value):
         config['influx_password'],
         config['influx_database'])
 
-    sampling_rate = config["sampling_rate"]
-    acceptable_offset = config["acceptable_offset"]
+    # sampling_rate = config["sampling_rate"]
+    # acceptable_offset = config["acceptable_offset"]
 
     # query metrics by tag
     query_metric = "SELECT * FROM \"%s\" where %s='%s' order by time asc" % \
                    (app_metric, tag_name, tag_value)
 
     show_measurements = "SHOW MEASUREMENTS"
+
+    print "preparing data..."
 
     rs_measurement = influxClient.query(show_measurements).items()
     measurement_list = [
@@ -47,7 +48,11 @@ def get_xgboost_data(app_metric, app_slo, tag_name, tag_value):
 
     rs_list = []
     for measurement in measurement_list:
-        rs_list.append({measurement: list(influxClient.query('select * from "%s"' % measurement, epoch='s').items()[0][GENERATOR_IDX])})
+        rs_list.append({
+            'key': measurement,
+            'generator': influxClient.query('select * from "%s"' % measurement, epoch='s').items()[0][GENERATOR_IDX],
+            'currentData': {}
+        })
 
     rs_app = influxClient.query(query_metric, epoch='s')
 
@@ -57,69 +62,68 @@ def get_xgboost_data(app_metric, app_slo, tag_name, tag_value):
 
     data = []
     proceedRecords = 0
-    next_sample_base = 0
+    previous_item_time = 0
 
     for tag in rs_app.items():
 
         # iterate per data point
+        print "scanning..."
         for data_point in tag[GENERATOR_IDX]:
 
             line = []
-            # find related metrics in rs with time interval 1s
             item_time = data_point['time']
-            if item_time < next_sample_base:
-                continue
-            else:
-                next_sample_base = item_time + sampling_rate
 
             print datetime.fromtimestamp(item_time)
 
-            # narrow this list
-            rs_list = [
-                {x.keys()[0]: binarySearch(x[x.keys()[0]], data_point, acceptable_offset, 0, len(x) - 1)}
-                for x in rs_list
-                if x
-            ]
-            found = []
+            for measurement_data in rs_list:
+                values = []
+                key_list = [
+                    key for key in keys
+                    if key["measurement"] == measurement_data["key"]]
+                if len(key) == 0:
+                    continue
 
-            # iterate for each key
-            for key in keys:
-                rs_group = [x[key["measurement"]] for x in rs_list if x.keys()[0] == key["measurement"]]
-                if len(rs_group) > 0 and rs_group[0] is not None:
-                    l = [x for x in rs_group[0] if x[key["tagKey"]] == key["tagValue"]]
-                    found = [x for x in l if fabs(x['time'] - item_time) <= acceptable_offset]
-                    if len(found) > 0:
-                        line.append("%d:%f" % (key["id"], mean([x['value'] for x in found])))
-            proceedRecords += 1
-            print(line)
+                while True:
+                    current_data = measurement_data['currentData']
+
+                    measurement_data['currentData'] = next(measurement_data['generator'], None)
+
+                    if current_data is None:
+                        break
+                    if not current_data:
+                        continue
+
+                    if current_data['time'] <= item_time and current_data['time'] > previous_item_time:
+                        if type(current_data['value']) is unicode or type(current_data['value']) is str:
+                            continue
+                        values.append(current_data['value'])
+                    else:
+                        # measurement_data time > item_time
+                        break
+                if len(values) > 0:
+                    line.append('%d:%f' % (key_list[0]['id'], mean(values)))
+
             if len(line) > 0:
-                data.append(line)
-            print "%s records proceed" % proceedRecords
+                line.append(str(data_point['value']))
+                print line
+                data.append(" ".join(line))
+            proceedRecords += 1
+            previous_item_time = item_time
+    print "total data: %d" % len(data)
+    print "write keys to key.txt"
+    write_to_file(keys, "keys.txt")
+    print "write data to data.txt"
+    write_to_file(data, "data.txt")
 
-    return XGBoostData(dict([{k['measurement'], k['id']} for k in keys]), data)
+    return XGBoostData(keys, data)
 
 
-def binarySearch(sorted_list, item, offset, lo, high):
-    """Narrow the sorted list by truncating data with timestamp less than item['time']."""
-    if not sorted_list:
-        return None
-    if len(sorted_list) == 0 or len(sorted_list) == 1:
-        return None
-    baseIndex = high / 2
-    base = sorted_list[baseIndex]
-
-    if fabs(base['time'] - item['time']) <= offset:
-        return sorted_list[lo:]
-    elif base['time'] < (item['time'] - offset):
-        newList = sorted_list[baseIndex:]
-        result = binarySearch(newList, item, offset, baseIndex, len(newList))
-    elif base['time'] > (item['time'] + offset):
-        result = binarySearch(sorted_list, item, offset, lo, baseIndex)
-
-    if result:
-        return sorted_list[lo:]
-    else:
-        return None
+def write_to_file(list, file_name):
+    """Write to file."""
+    with open(file_name, 'w') as text_file:
+        for item in list:
+            text_file.write(str(item))
+            text_file.write("\n")
 
 
 def generateKeys(influxClient, rs):
@@ -144,7 +148,7 @@ def generateKeys(influxClient, rs):
     keys = []
     index = 0
     for x in rs:
-        measurement_name = x.keys()[0]
+        measurement_name = x['key']
         item = [k for k in tagKey if k in measurement_name]
         if len(item) == 1:
             tagQuery = 'SHOW TAG VALUES FROM "%s" WITH KEY="%s"' % (measurement_name, tagKey[item[0]])
@@ -173,5 +177,5 @@ if __name__ == '__main__':
         tag_name="method",
         tag_value="load")
 
-    print d.keys
-    print d.data
+    # print d.keys
+    # print d.data
