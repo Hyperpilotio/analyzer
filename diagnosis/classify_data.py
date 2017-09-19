a43f60297cc9a7db07d93742d500ce404c96d2a4
adrianliaw <adrianliaw2000@gmail.com>
2017-09-19 16:34:43 +0800

    Use correct data types

--- diagnosis/classify_data.py
+++ diagnosis/classify_data.py
@@ -4,13 +4,13 @@ import numpy as np
 
 SLO_VALUE = 100
 
-arr = np.loadtxt('data.txt')
+arr = np.loadtxt('data.txt', dtype=str)
 
-y = arr[:, 0]
-ybin = y <= SLO_VALUE
+y = arr[:, 0].astype(float)
+ybin = (y <= SLO_VALUE).astype(int)
 
 output = np.column_stack((ybin, arr[:, 1:-1]))
-np.savetxt('data-class.txt', output, fmt='%d')
+np.savetxt('data-class.txt', output, fmt='%s')
 
 print("maximum y value = %d" % y.max())
 print("minimum y value = %d" % y.min())
