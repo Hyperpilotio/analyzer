9bb0fcafe02c4c2e90ad1b80e5e6893d1c31d7a9
Cherie Meyer <732149+cherplunk@users.noreply.github.com>
2017-11-16 15:46:12 -0800

    Organize metric consumer inputs/options.

--- diagnosis/metric_consumer.py
+++ diagnosis/metric_consumer.py
@@ -117,17 +117,12 @@ class MetricConsumer(object):
         return df
 
     def get_derived_input_data(self):
-        nodeAnalyzer = DerivedMetrics("./derived_metrics_config.json")
+        nodeAnalyzer = DerivedMetrics("./diagnosis/derived_metrics_config.json")
         return nodeAnalyzer.get_derived_metrics(
             -9223372036854775806, 9223372036854775806)
 
     def compute_correlation(self):
-        print("input df")
-        print(self.input_df.shape)
-        print("sl df")
-        print(self.sl_df.shape)
         correlations = self.input_df.corrwith(self.sl_df[app_metric])
-        print(correlations)
         print(correlations.sort_values(ascending=False))
 
     def get_end_time_from_db(self):
@@ -144,76 +139,6 @@ class MetricConsumer(object):
         # todo: take in computation window
         start_time = end_time - 5 * 60 * M
         return "time > %d" % start_time
-
-    def get_data(self, start_time=None, end_time=None, metric_type="raw"):
-        """ Returns a dataframe of cleaned, interpolated data for the time specified.
-        If no period is specified, it returns a dataframe from the last five minutes
-        of influx data found. """
-
-        tag_filter = " AND " .join(["%s='%s'" % (k, v)
-                                    for k, v in tags.items()])
-        q = "select last(*) from \"%s\" where %s" % \
-            (app_metric, tag_filter)
-        last_sample = df_client_app.query(q)
-
-        last_time = last_sample[app_metric].index[0].timestamp() * M
-        # set the end time to now (or just past the last timestamp in the data)
-        end_time = last_time + M
-        # print(end_time)
-        # if 'start_time' in config and config['start_time'] != "":
-        #    time_filter = "time > %d" % int(config['start_time'])
-        # else:
-        start_time = end_time - 5 * 60 * M
-        time_filter = "time > %d" % start_time
-
-        # if 'end_time' in config and config['end_time'] != "":
-        #time_filter = "%s AND time < %d" % (time_filter, start_time + 5 * 60 * M)
-
-        time_filter = "%s AND time < %d" % (time_filter, end_time)
-        logger.info('Using time filter: ' + time_filter)
-
-        # query metrics by tag and time filters
-        query_metric = "select value from \"%s\" where %s AND %s order by time desc" % \
-            (app_metric, tag_filter, time_filter)
-        logger.info("Query to get app metric: \n " + query_metric)
-        sl_data = df_client_app.query(query_metric)[app_metric]
-
-        N = len(sl_data)
-
-        time_buckets = range(int(end_time), int(start_time), -5 * M)
-        df = self.match_timestamps(time_buckets, sl_data)
-        df.columns = [app_metric]
-
-        logger.info("Number of app metric samples fetched: %d" % len(sl_data))
-
-        show_measurements = "SHOW MEASUREMENTS"
-
-        rs_measurement = df_client_node.query(show_measurements)
-        measurement_list = [
-            x['name'] for x in rs_measurement['measurements']
-            if "hyperpilot" not in x['name'] and x['name'] not in ds.EXCLUDE_MEASUREMENTS
-        ]
-        logger.info("Number of system metrics to be fetched: %d" %
-                    len(measurement_list))
-
-        for measurement in measurement_list:
-            measurements_query = 'select value from "%s" where %s order by time desc' % (
-                measurement, time_filter)
-            logger.info("Query to get system metric: \n " + measurements_query)
-            result = df_client_node.query(measurements_query)
-            if measurement not in result:
-                logger.info("No items found for measurement %s" % measurement)
-                continue
-            node_data = result[measurement]
-
-            if type(node_data.iloc[0, 0]) == str:
-                logger.info("Query incorrect for measurement %s" % measurement)
-                continue
-
-            df[measurement] = self.match_timestamps(
-                time_buckets, node_data).iloc[:, 0]
-
-        return df.interpolate()
 
     def shift_and_update(self):
         new_df = self.get_data()
@@ -258,5 +183,5 @@ class MetricConsumer(object):
 
 if __name__ == "__main__":
     MetricConsumer("RAW", "RAW")
-    #mc = MetricConsumer("RAW", "DERIVED")
+    MetricConsumer("RAW", "DERIVED")
     #mc = MetricConsumer("DERIVED", "DERIVED")
