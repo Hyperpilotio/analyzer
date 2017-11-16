a705440d02ee4c20d18a7f7ebf38b7328f7837fb
Cherie Meyer <732149+cherplunk@users.noreply.github.com>
2017-11-16 15:43:45 -0800

    Organize metric consumer inputs/options.

--- diagnosis/app_analyzer.py
+++ diagnosis/app_analyzer.py
@@ -1,4 +1,6 @@
 import time
+from collections import namedtuple
+
 
 from diagnosis.metric_consumer import MetricConsumer
 from config import get_config
@@ -7,16 +9,29 @@ config = get_config()
 BATCH_TIME = int(config.get("ANALYZER", "CORRELATION_BATCH_TIME"))
 WINDOW = int(config.get("ANALYZER", "CORRELATION_WINDOW"))
 logger = get_logger(__name__, log_level=("ANALYZER", "LOGLEVEL"))
+tags = {"method": "request_routes", "summary": "quantile_90"}
+METRIC_TYPES = set(["RAW", "DERIVED"])
+
+class AppAnalyzer(object):
+    def __init__(self, sl_metric, sl_metric_type, input_metric_type, start_time=None, end_time=None, sl_metric_json=None):
+        if sl_metric_type not in METRIC_TYPES or input_metric_type not in METRIC_TYPES:
+            raise ValueError("Supported metric types are RAW and DERIVED.")
+        self.sl_metric = sl_metric
+        if sl_metric_type == "DERIVED":
+            if sl_metric_json == None:
+                raise ValueError("Derived SL metric must be accompanied by SL metric json.")
+        if sl_metric_type == "DERIVED" and input_metric_type == "RAW":
+            raise ValueError("Combination of input and SL metric types not supported.")
+
+        self.metric_consumer = MetricConsumer(sl_metric, sl_metric_type, input_metric_type)
+        return self.metric_consumer.compute_correlation()
 
 
 if __name__ == "__main__":
-    # for testing purposes: 
-    mc = MetricConsumer()
-    # with running snap collectors:
-    # mc = Metric_Consumer(end_time=now, start_time=now-5min)
+    aa = AppAnalyzer("hyperpilot/goddd/api_booking_service_request_latency_microseconds", "RAW", "RAW")
 
     while True:
         time.sleep(WINDOW)
-        mc.shift_and_update()
-        mc.write_result()
+        aa.metric_consumer.shift_and_update()
+        aa.metric_consumer.write_result()
         logger.info("Wrote correlation coefficient to result db.")
