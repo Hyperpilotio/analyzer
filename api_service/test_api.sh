c47f9ca2302f8485fb264029ee99c5e9b9095258
Xiaoyun Zhu <xiaoyzhu@yahoo.com>
2017-11-17 12:53:49 -0800

    change services to microservices in application json

--- api_service/test_api.sh
+++ api_service/test_api.sh
@@ -4,7 +4,7 @@
 curl -XGET localhost:5000/api/apps
 
 # create app
-APP_ID=$(curl -s  -H  "Content-Type: application/json" -X POST --data-binary "@workloads/tech-demo-app.json" localhost:5000/api/apps | grep data | cut -d " " -f4  | cut -d "\"" -f2) ; echo $APP_ID
+APP_ID=$(curl -s  -H  "Content-Type: application/json" -X POST --data-binary "@workloads/tech-demo-app.json" localhost:5000/api/apps | grep data | cut -d " " -f4  | cut -d "\"" -f2) ; echo "app_id in new app: $APP_ID"
 
 # require type and name (this example has no type)
 curl -s  -H  "Content-Type: application/json" -X POST --data-binary "@workloads/tech-demo-bad-input.json" localhost:5000/api/apps
@@ -12,11 +12,11 @@ curl -s  -H  "Content-Type: application/json" -X POST --data-binary "@workloads/
 # get one app by id
 curl -XGET localhost:5000/api/apps/$APP_ID
 
-# modify app
+# update an existing app
 curl -H "Content-Type: application/json" -X PUT localhost:5000/api/apps/$APP_ID --data-binary "@workloads/tech-demo-partial-update.json"
 
 # delete an app by id 
 curl -XDELETE localhost:5000/api/apps/$APP_ID
 
-# add services to an application
-curl -H "Content-Type: application/json" -X POST localhost:5000/api/apps/$APP_ID/services --data-binary "@workloads/services.json"
+# add microservices to an app
+curl -H "Content-Type: application/json" -X POST localhost:5000/api/apps/$APP_ID/services --data-binary "@workloads/microservices.json"
