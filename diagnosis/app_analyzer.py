f90f4aa31bd001337100e7c1d34eb52d1ac72d29
Timothy Chen <tnachen@gmail.com>
2017-11-21 13:37:26 -0800

    Fix metrics consumer from derived metrics

--- diagnosis/app_analyzer.py
+++ diagnosis/app_analyzer.py
@@ -1,37 +1,33 @@
 import time
 from collections import namedtuple
 
-
-from diagnosis.metric_consumer import MetricConsumer
+from diagnosis.derived_metrics import MetricsConsumer
+from diagnosis.diagnosis import MetricsConsumer
 from config import get_config
 
 config = get_config()
-BATCH_TIME = int(config.get("ANALYZER", "CORRELATION_BATCH_TIME"))
 WINDOW = int(config.get("ANALYZER", "CORRELATION_WINDOW"))
 logger = get_logger(__name__, log_level=("ANALYZER", "LOGLEVEL"))
-tags = {"method": "request_routes", "summary": "quantile_90"}
 METRIC_TYPES = set(["RAW", "DERIVED"])
 
 class AppAnalyzer(object):
-    def __init__(self, sl_metric, sl_metric_type, input_metric_type, start_time=None, end_time=None, sl_metric_json=None):
-        if sl_metric_type not in METRIC_TYPES or input_metric_type not in METRIC_TYPES:
-            raise ValueError("Supported metric types are RAW and DERIVED.")
-        self.sl_metric = sl_metric
-        if sl_metric_type == "DERIVED":
-            if sl_metric_json == None:
-                raise ValueError("Derived SL metric must be accompanied by SL metric json.")
-        if sl_metric_type == "DERIVED" and input_metric_type == "RAW":
-            raise ValueError("Combination of input and SL metric types not supported.")
+    def __init__(self, config):
+        self.config = config
 
-        self.metric_consumer = MetricConsumer(sl_metric, sl_metric_type, input_metric_type)
-        return self.metric_consumer.compute_correlation()
+    def loop_all_app_metrics(self, start_time, batch_window):
+        mc = MetricsConsumer("./derived_slo_metric_config.json", "./derived_metrics_config.json")
+        #d = Diagnosis()
 
+        while True:
+            end_time = start_time + batch_window
+            derived_metrics = mc.get_derived_metrics(start_time, end_time)
+            if len(derived_metrics.app_metrics.index) == 0:
+                print("No more app metrics, exiting..")
+                return
+            metrics_with_cs = d.process_metrics(derived_metrics)
+            print(metrics_with_cs)
+            start_time += batch_window
 
 if __name__ == "__main__":
-    aa = AppAnalyzer("hyperpilot/goddd/api_booking_service_request_latency_microseconds", "RAW", "RAW")
-
-    while True:
-        time.sleep(WINDOW)
-        aa.metric_consumer.shift_and_update()
-        aa.metric_consumer.write_result()
-        logger.info("Wrote correlation coefficient to result db.")
+    aa = AppAnalyzer()
+    #aa.loop_all_app_metrics()
