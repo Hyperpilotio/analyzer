6ad738430c2be4c721fc3cde63c0c8b93a319bc9
Timothy Chen <tnachen@gmail.com>
2017-12-19 14:26:14 -0800

    Run diagnosis live

--- diagnosis/app_analyzer.py
+++ diagnosis/app_analyzer.py
@@ -18,6 +18,7 @@ from logger import get_logger
 config = get_config()
 WINDOW = int(config.get("ANALYZER", "CORRELATION_WINDOW_SECOND"))
 INTERVAL = int(config.get("ANALYZER", "DIAGNOSIS_INTERVAL_SECOND"))
+DELAY_INTERVAL = int(config.get("ANALYZER", "DELAY_INTERVAL_SECOND"))
 AVERAGE_WINDOW = int(config.get("ANALYZER", "AVERAGE_WINDOW_SECOND"))
 severity_compute_type = config.get("ANALYZER", "SEVERITY_COMPUTE_TYPE")
 if severity_compute_type == "AREA":
@@ -50,70 +51,88 @@ class AppAnalyzer(object):
             influx_db)
         self.influx_client.create_retention_policy('result_policy', '3w', 1, default=True)
 
+    def diagnosis_cycle(self, app_name, start_time, end_time):
+        app_metric = self.metrics_consumer.get_app_metric(start_time, end_time, is_derived=True)
+        if app_metric is None:
+            logger.info("No app metric found, exiting diagnosis...")
+            return False
+        window = int(config.get("ANALYZER", "AVERAGE_WINDOW_SECOND")) * NANOSECONDS_PER_SECOND
+        window_start = to_datetime(end_time - window, unit="ns")
+        app_metric_mean = app_metric.loc[app_metric.index >= window_start].mean()
+        if app_metric_mean["value"] < DIAGNOSIS_THRESHOLD:
+            logger.info("Derived app metric mean: %f below threshold %f; skipping diagnosis..." %
+                        (app_metric_mean["value"], DIAGNOSIS_THRESHOLD))
+        else:
+            logger.info("Derived app metric mean: %f above threshold %f; starting diagnosis..." %
+                        (app_metric_mean["value"], DIAGNOSIS_THRESHOLD))
+
+        derived_metrics = self.metrics_consumer.get_derived_metrics(start_time, end_time,
+                                                                            app_metric)
+        incident_id = "incident" + "-" + str(uuid1())
+        incident_doc = {"incident_id": incident_id,
+                        "type": self.metrics_consumer.incident_type,
+                        "labels": {"app_name": app_name},
+                        "metric": self.metrics_consumer.incident_metric,
+                        "threshold": self.metrics_consumer.incident_threshold,
+                        "severity": app_metric_mean["value"],
+                        "timestamp": end_time}
+        RESULTDB[incidents_collection].insert_one(incident_doc)
+
+        filtered_metrics = self.features_selector.process_metrics(derived_metrics)
+        if not filtered_metrics:
+            logger.info("All %d features have been filtered." % self.features_selector.num_features)
+            it += 1
+            return True
+
+        self.write_results(filtered_metrics, end_time, app_name,
+                           self.metrics_consumer.deployment_id)
+
+        # Sort top k derived metrics based on conficent score
+        sorted_metrics = sorted(filtered_metrics, key=lambda x: self.convert_nan(
+            x.confidence_score), reverse=True)[:10]
+        logger.info("Top related metrics for incident %s for application %s:" %
+                    (incident_id, app_name))
+        self.print_sorted_metrics(sorted_metrics)
+
+        # Identify top problems and generate diagnosis result
+        logger.info("\nStart generating diagnosis for incident %s for application %s:" %
+                    (incident_id, app_name))
+        self.diagnosis_generator.process_features(
+            sorted_metrics, app_name, incident_id, end_time)
+
+        return True
+
+    def run(self, batch_window, sliding_interval, delay_interval):
+        app_name = config.get("ANALYZER", "APP_NAME")
+        logger.info("Starting live diagnosis run for application %s" % app_name)
+        end_time = datetime.datetime.now() - delay_interval
+        start_time = end_time - batch_window
+        while True:
+            start_run_time = datetime.datetime.now()
+            self.diagnosis_cycle(app_name, start_time, end_time)
+            diagnosis_time = datetime.datetime.now() - start_run_time
+            logger.info("Diagnosis cycle took %s" % diagnosis_time)
+            sleep_time = (sliding_interval - (diagnosis_time.total_seconds() * NANOSECONDS_PER_SECOND) * 1.) / NANOSECONDS_PER_SECOND
+            if sleep_time > 0:
+                logger.info("Sleeping for %f before next cycle" % sleep_time)
+                usleep(sleep_time)
+
     def loop_all_app_metrics(self, end_time, batch_window, sliding_interval):
         it = 1
+        app_name = config.get("ANALYZER", "APP_NAME")
         while True:
             start_time = end_time - batch_window
             logger.info("\nIteration %d - Processing metrics from start: %d, to end: %d" %
                   (it, start_time, end_time))
-            app_metric = self.metrics_consumer.get_app_metric(start_time, end_time, is_derived=True)
-            if app_metric is None:
-                logger.info("No app metric found, exiting diagnosis...")
+            if self.diagnosis_cycle(app_name, start_time, end_time) == False:
                 return
-            window = int(config.get("ANALYZER", "AVERAGE_WINDOW_SECOND")) * NANOSECONDS_PER_SECOND
-            window_start = to_datetime(end_time - window, unit="ns")
-            app_metric_mean = app_metric.loc[app_metric.index >= window_start].mean()
-            if app_metric_mean["value"] < DIAGNOSIS_THRESHOLD:
-                logger.info("Derived app metric mean: %f below threshold %f; skipping diagnosis..." %
-                      (app_metric_mean["value"], DIAGNOSIS_THRESHOLD))
-            else:
-                logger.info("Derived app metric mean: %f above threshold %f; starting diagnosis..." %
-                      (app_metric_mean["value"], DIAGNOSIS_THRESHOLD))
-
-                derived_metrics = self.metrics_consumer.get_derived_metrics(start_time, end_time,
-                                                                            app_metric)
-                incident_id = "incident" + "-" + str(uuid1())
-                app_name = config.get("ANALYZER", "APP_NAME")
-                incident_doc = {"incident_id": incident_id,
-                                "type": self.metrics_consumer.incident_type,
-                                "labels": {"app_name": app_name},
-                                "metric": self.metrics_consumer.incident_metric,
-                                "threshold": self.metrics_consumer.incident_threshold,
-                                "severity": app_metric_mean["value"],
-                                "timestamp": end_time}
-                RESULTDB[incidents_collection].insert_one(incident_doc)
-
-                filtered_metrics = self.features_selector.process_metrics(derived_metrics)
-                if not filtered_metrics:
-                    logger.info("All %d features have been filtered." % self.features_selector.num_features)
-                    it += 1
-                    continue
-
-                self.write_results(filtered_metrics, end_time, app_name,
-                                   self.metrics_consumer.deployment_id)
-
-                # Sort top k derived metrics based on conficent score
-                sorted_metrics = sorted(filtered_metrics, key=lambda x: self.convertNaN(
-                                        x.confidence_score), reverse=True)[:10]
-                logger.info("Top related metrics for incident %s for application %s:" %
-                       (incident_id, app_name))
-                self.print_sorted_metrics(sorted_metrics)
-
-                # Identify top problems and generate diagnosis result
-                logger.info("\nStart generating diagnosis for incident %s for application %s:" %
-                       (incident_id, app_name))
-                self.diagnosis_generator.process_features(
-                    sorted_metrics, app_name, incident_id, end_time)
-
             end_time += sliding_interval
             it += 1
 
-
-    def convertNaN(self, value):
+    def convert_nan(self, value):
         if math.isnan(value):
             return 0.0
         return value
-
 
     def write_results(self, metrics, end_time, app_name, deployment_id):
         points_json = []
@@ -148,7 +167,6 @@ class AppAnalyzer(object):
 
 
     def print_sorted_metrics(self, sorted_metrics):
-
         i = 1
         for m in sorted_metrics:
             logger.info("\nRank: " + str(i) + "\n" +
@@ -167,5 +185,8 @@ class AppAnalyzer(object):
 
 if __name__ == "__main__":
     aa = AppAnalyzer(config)
-    aa.loop_all_app_metrics(1511980830000000000, WINDOW * NANOSECONDS_PER_SECOND, INTERVAL * NANOSECONDS_PER_SECOND)
+    if len(sys.argv) > 1:
+        aa.run(WINDOW * NANOSECONDS_PER_SECOND, INTERVAL * NANOSECONDS_PER_SECOND)
+    else:
+        aa.loop_all_app_metrics(1511980830000000000, WINDOW * NANOSECONDS_PER_SECOND, INTERVAL * NANOSECONDS_PER_SECOND)
     #aa.loop_all_app_metrics(1513062600000000000, WINDOW * NANOSECONDS_PER_SECOND, INTERVAL * NANOSECONDS_PER_SECOND)
